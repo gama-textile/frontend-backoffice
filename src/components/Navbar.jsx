38dbc132e732bc7ac5a5df69ec74ab519e30ab76
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2019-04-09 21:11:05 +0700

    solved upload image to react

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <h1>ini navbar</h1>
+      </div>
+    );
+  }
+}
